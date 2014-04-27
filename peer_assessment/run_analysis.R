eb7f8ecca14b8e38a044a343940b7147c3c55047
Xiaodan Zhang <zhang349@illinois.edu>
2014-04-26 21:41:09 -0500

    modified run_analysis.R

--- peer_assessment/run_analysis.R
+++ peer_assessment/run_analysis.R
@@ -69,3 +69,6 @@ for(i in 1:subjectLen) {
 }
 head(result)
 write.table(result, "data_with_means.txt") # write out the 2nd dataset
+
+# data <- read.table("./data_with_means.txt")
+# data[1:12, 1:3]
